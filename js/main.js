16291300d25399397e30cb123d87e69297a47c1f
ManganiBiagio <biagiomangani@live.it>
2022-10-27 14:20:50 +0200

    funzione calcolo prezzo biglietto

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const inputKm=document.getElementById("km");
+const inputAge=document.getElementById("age");
+const outputSpan=document.getElementById("output-span");
+const bottoneForm=document.getElementById("bottone-form");
+const boxOutput=document.getElementById("box-output");
+
+bottoneForm.addEventListener("click",()=> PriceTicket());
+
+function PriceTicket(){
+    let age=parseInt(inputAge.value);
+    let price=(parseInt(inputKm.value))* 0.21;
+    
+    if(isNaN(price)){
+        alert("il numero di km inserito non è valido");
+    }
+    else{
+        if(isNaN(age)){
+            alert("ricordati di inserire un età");
+        }
+        else{
+            if(age<=18){
+                price*=0.8;
+    
+            }
+            else if(age>=65){
+                price*=0.6;
+            }
+            outputSpan.innerHTML=price.toFixed(2);
+
+        }
+        
+
+
+    }
+    
+}
